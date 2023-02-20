71eae1d7eef91150cb998cd2a6289a76fb8f9811
frankie_em <jorge.lopez@car.fit>
2023-02-20 15:08:08 +0100

    Add first model, db and unitary tests

---
+++ PlantApp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from PlantApp.models import Plant
+
+# Register the model created to check on the admin page
+admin.site.register(Plant)
---
+++ PlantApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from PlantApp import views
+
+urlpatterns = [
+    # path('', views., name="manage_plant")
+]
